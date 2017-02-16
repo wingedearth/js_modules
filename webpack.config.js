226674d1a9bb44127b92689a556e9edf51554528
Andrew Anissi <aanissi@legacy.com>
2017-02-15 18:29:48 -0800

    add publicPath to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const config = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'build/'
   },
   module: {
     rules: [
